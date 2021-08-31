24faf17c727ce387127c7e92bb8a4b2b1d004239
elliottwu <elliottwu@users.noreply.github.com>
2021-08-31 03:58:40 +0100

    release dataset and experiments on synthetic cars

---
+++ pretrained/download_pretrained_syncar.sh
@@ -0,0 +1,2 @@
+echo "----------------------- downloading pretrained model on synthetic car dataset -----------------------"
+curl -o pretrained_syncar.zip "https://www.robots.ox.ac.uk/~vgg/research/unsup3d/data/pretrained_syncar.zip" && unzip pretrained_syncar.zip
